82249919c2d11f69df41d65a9f5135bac61af7e3
kli19 <kli19@stuy.edu>
2018-11-16 15:34:42 -0500

    rand_int btw 5 and 20

--- forkin.c
+++ forkin.c
@@ -1,17 +1,52 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <sys/wait.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
-int main(){
-  int f = fork();
-
+void forking(int f){
   if (f == -1) {
     printf("error");
   } else if(f) {
     printf("Parent of %i pid: %i ppid: %i\n", f, getpid(), getppid());
   } else {
-    printf("I am a child pid: %i ppid: %i\n", getpid(), getppid());
+    printf("I am a child pid: %i ppid: %i\n\n", getpid(), getppid());
   }
+}
+
+
+int rand_num(){
+  int random_data = open("/dev/random", O_RDONLY);
+  int * p = (int *)malloc(sizeof(int));
+  read(random_data, p, sizeof(p));
+  int num = abs(*p);
+  free(p);
+  if (close(random_data) < 0){
+    printf("ERROR CLOSING FILE");
+  };
+  while(num>15){
+    num = num%20;
+    num+=5;
+  }
+  return num;
+}
+
+
+int main(){
+  printf("%d\n", rand_num());
+  printf("%d\n", rand_num());
+  printf("%d\n", rand_num());
+  printf("%d\n", rand_num());
+  /*
+  int a = fork();
+  forking(a);
+
+  int b = fork();
+  forking(b);
+
+  int c = fork();
+  forking(c);
+  */
 
   return 0;
 }
