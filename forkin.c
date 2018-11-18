56a0904a685e04bdf12d24038e47ca2d8f32b1ae
Karen Li <kli19@stuy.edu>
2018-11-17 22:50:57 -0500

    refactoring code and comments

--- forkin.c
+++ forkin.c
@@ -4,6 +4,7 @@
 # include <stdlib.h>
 # include <fcntl.h>
 
+//returns a random integer between 5 and 20
 int rand_num(){
   int random_data = open("/dev/random", O_RDONLY);
   int * p = (int *)malloc(sizeof(int));
@@ -15,28 +16,39 @@ int rand_num(){
   };
   return num%16+5;
 }
-int main(){
-  
-  int id = getpid();
-  fork();
 
-  if (id == -1) {
+int main(){
+  //initial message before forking
+  printf("I am the parent process pid: %d ppid: %d\n", getpid(), getppid());
+  
+  //fork the parent
+  int f = fork();
+
+  //error
+  if (f == -1) {
     printf("error");
   }
-  if(id==getpid()) {
-    fork();
+
+  //fork again if parent
+  if(f) {
+    f = fork();
   }
-  if(id!=getpid()){
+
+  //sleep if child
+  if(f==0){
     printf("I am a sleepy child pid: %i ppid: %i\n", getpid(), getppid());
     int nap_time = rand_num();
     sleep(nap_time);
     printf("Sleepy child is awake now\n");
     return nap_time;
   }
+
+  //parent process stuff
   int status;
   int sleepy_child = wait(&status);
   int seconds = WEXITSTATUS(status);
   printf("My sleepy child %d slept for %d seconds\n", sleepy_child, seconds);
+  printf("Parent process is done!\n");
 
   return 0;
 }
