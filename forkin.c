f7bdf1af63a556d1da1bc6a9442a969f8ad7d09c
Karen Li <kli19@stuy.edu>
2018-11-17 21:31:05 -0500

    why is child pid -1

--- forkin.c
+++ forkin.c
@@ -4,7 +4,6 @@
 # include <stdlib.h>
 # include <fcntl.h>
 
-
 int rand_num(){
   int random_data = open("/dev/random", O_RDONLY);
   int * p = (int *)malloc(sizeof(int));
@@ -14,26 +13,29 @@ int rand_num(){
   if (close(random_data) < 0){
     printf("ERROR CLOSING FILE");
   };
-  while(num>15){
-    num = num%20;
-    num+=5;
-  }
-  return num;
+  return num%16+5;
 }
-
-
 int main(){
   
   int f = fork();
 
-    if (f == -1) {
+  if (f == -1) {
     printf("error");
-  } else if(f) {
-    printf("Parent of %i pid: %i ppid: %i\n", f, getpid(), getppid());
-    //f=fork();
-  } else {
-    printf("I am a child pid: %i ppid: %i\n\n", getpid(), getppid());
   }
-
+  if(f) {
+    fork();
+  }
+  if(f==0){
+    printf("I am a sleepy child pid: %i ppid: %i\n\n", getpid(), getppid());
+    int nap_time =  2;
+    sleep(nap_time);
+    printf("Sleepy child is awake now\n");
+    return nap_time;
+  }
+  int status;
+  int sleepy_child = wait(&status);
+  int sec = WEXITSTATUS(sleepy_child);
+  printf("My sleepy child %d slept for %d seconds\n", sleepy_child, sec);
+  
   return 0;
 }
